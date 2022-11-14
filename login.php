a638aa759924694d4c2131a6cf11ab061b66d7d8
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-14 20:03:09 +0100

    add style and inputs in form - work out

--- login.php
+++ login.php
@@ -5,22 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
-    <style>
-        body{
-            background-color: #F1F1F1;
-        }
-        .card-box{
-            background-color: #E7E7E7;
-        }
-    </style>
     <section class="container">
         <div class="card-box col-12 col-md-6 mx-auto">
             <div class="mb-3">
-                <label for="price" class="form-label">Price</label>
-                <input type="text" name="price" id="price" class="form-control" placeholder="Add product price" required>
+                <label for="username" class="form-label">User Name</label>
+                <input type="text" name="username" id="username" class="form-control" placeholder="Add Your User Name" min="4" max="50" required>
             </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" name="password" id="password" class="form-control" placeholder="******" min="8" max="50" required>
+            </div>
+            <a href="#">Forgot your password?</a><br><br>
+            <button type="submit" class="btn btn-primary col-12">Sign In</button>
         </div>
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
