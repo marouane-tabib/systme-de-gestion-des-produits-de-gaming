6497b069c5dce7fb509d6826755c6f480155ab9a
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-14 17:03:32 +0100

    add required in model form inputs

--- includes/layouts/model.php
+++ includes/layouts/model.php
@@ -11,40 +11,40 @@
         <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-        <form action="index.php" method="post">
-          <div class="mb-3">
-            <label for="img_url" class="form-label">Image URL</label>
-            <input type="url" class="form-control" id="img_url" name="img_url" placeholder="Add Image URL (https://www.example.com)">
-          </div>
-          <div class="mb-3">
-            <label for="name" class="form-label">Product Name</label>
-            <input type="text" class="form-control" id="name" name="name" placeholder="Add product name...">
-          </div>
-          <div class="mb-3">
-            <label for="quantity" class="form-label">Quantity</label>
-            <input type="text" class="form-control" id="quantity" name="quantity" placeholder="Add product quantity">
-          </div>
-          <div class="mb-3">
-            <label for="platform" class="form-label">Platform</label>
-            <select name="platform" id="platform" class="form-control">
-              <option value="steam">Steam</option>
-            </select>
-          </div>
-          <div class="mb-3">
-            <label for="price" class="form-label">Price</label>
-            <input type="text" name="price" id="price" class="form-control" placeholder="Add product price">
-          </div>
-          <div class="mb-3">
-            <label for="description" class="form-label">Description</label>
-            <textarea name="description" id="description" class="form-control" rows="3"></textarea>
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Send</button>
-      </div>
+      <form action="index.php" method="post">
+        <div class="modal-body">
+            <div class="mb-3">
+              <label for="img_url" class="form-label">Image URL</label>
+              <input type="url" class="form-control" id="img_url" name="img_url" placeholder="Add Image URL (https://www.example.com)" required>
+            </div>
+            <div class="mb-3">
+              <label for="name" class="form-label">Product Name</label>
+              <input type="text" class="form-control" id="name" name="name" placeholder="Add product name..." required>
+            </div>
+            <div class="mb-3">
+              <label for="quantity" class="form-label">Quantity</label>
+              <input type="text" class="form-control" id="quantity" name="quantity" placeholder="Add product quantity" required>
+            </div>
+            <div class="mb-3">
+              <label for="platform" class="form-label">Platform</label>
+              <select name="platform" id="platform" class="form-control" required>
+                <option value="steam">Steam</option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <label for="price" class="form-label">Price</label>
+              <input type="text" name="price" id="price" class="form-control" placeholder="Add product price" required>
+            </div>
+            <div class="mb-3">
+              <label for="description" class="form-label">Description</label>
+              <textarea name="description" id="description" class="form-control" rows="3" required></textarea>
+            </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary">Send</button>
+        </div>
+      </form>
     </div>
   </div>
 </div>
