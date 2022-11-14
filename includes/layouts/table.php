d890f6ce2e53be91f6e90f6a3fb67c08b6bda9d5
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-14 15:28:42 +0100

    add includes foldes , create model and table include file

---
+++ includes/layouts/table.php
@@ -0,0 +1,52 @@
+
+        <table class="table">
+            <thead class="table-dark">
+                <tr>
+                <th scope="col">Image</th>
+                <th scope="col">ID</th>
+                <th scope="col">Game Name</th>
+                <th scope="col">QTY</th>
+                <th scope="col">Platform</th>
+                <th scope="col">Price</th>
+                <th scope="col" id="description">Description</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                <tr>
+                    <th class="col-2"><img src="https://cdn.tutsplus.com/cdn-cgi/image/width=600/psd/uploads/legacy/psdtutsarticles/linkb_60vgamecovers/35.jpg" width="70"></th>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th class="col-2"><img src="https://www.oxpal.com/wp-content/uploads/2014/08/far_cry_3_-_cover.jpg" width="70"></th>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th class="col-2"><img src="https://m.media-amazon.com/images/I/61xfJLv0DJS._AC_SX385_.jpg" width="70"></th>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th class="col-2"><img src="https://interfaceingame.com/wp-content/uploads/elden-ring/elden-ring-cover-375x500.jpg" width="70"></th>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                </tr>
+            </tbody>
+        </table>
--- index.php
+++ index.php
@@ -4,65 +4,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
     <h1>Hello, world!</h1>
     <section class="container">
-        <table class="table">
-            <thead class="table-dark">
-                <tr>
-                <th scope="col">Image</th>
-                <th scope="col">ID</th>
-                <th scope="col">Game Name</th>
-                <th scope="col">QTY</th>
-                <th scope="col">Platform</th>
-                <th scope="col">Price</th>
-                <th scope="col" id="description">Description</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <tr>
-                    <th class="col-2"><img src="https://cdn.tutsplus.com/cdn-cgi/image/width=600/psd/uploads/legacy/psdtutsarticles/linkb_60vgamecovers/35.jpg" width="70"></th>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th class="col-2"><img src="https://www.oxpal.com/wp-content/uploads/2014/08/far_cry_3_-_cover.jpg" width="70"></th>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th class="col-2"><img src="https://m.media-amazon.com/images/I/61xfJLv0DJS._AC_SX385_.jpg" width="70"></th>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th class="col-2"><img src="https://interfaceingame.com/wp-content/uploads/elden-ring/elden-ring-cover-375x500.jpg" width="70"></th>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-            </tbody>
-        </table>
+        <?php include"includes/layouts/table.php"; ?>
     </section>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     <script src="assets/js/main.js"></script>
   </body>
 </html>
