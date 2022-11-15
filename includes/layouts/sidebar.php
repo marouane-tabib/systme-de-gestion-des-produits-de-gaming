22bd6e3f01999a9b2efbf0509f609cf5366e345e
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-15 11:28:55 +0100

    add sidbar in includes layouts files - pause task

---
+++ includes/layouts/sidebar.php
@@ -0,0 +1,21 @@
+
+      <div class="side-bar">
+        <div class="side-box">
+          <div class="side-items">
+            <ul>
+              <li>
+                <a href="#">
+                  <span class="slide-icon"><i class="fa-solid fa-box-open"></i></span>
+                  <span class="side-page">Products</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span class="slide-icon"><i class="fa-thin fa-camera"></i></span>
+                  <span class="side-page">Images</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
