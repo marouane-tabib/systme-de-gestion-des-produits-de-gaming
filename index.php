45b18728c789266a009255aac416c04970e9ffb8
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-15 13:19:24 +0100

    add session checkout in index - work out

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
+<?php
+    // Redirect
+    session_start();
+    if(!$_SESSION['user']){ header('location:login.php'); }
+    //--- Header include
+    $title = "Home";
+    include"includes/layouts/header.php";
 
-        <?php
-        //--- Header include
-        $title = "Home";
-        include"includes/layouts/header.php";
+    //--- Body includes
+      include"includes/layouts/logo.php"; 
+      include"includes/layouts/model.php"; 
+      include"includes/layouts/table.php";
 
-        //--- Body includes
-          include"includes/layouts/logo.php"; 
-          include"includes/layouts/model.php"; 
-          include"includes/layouts/table.php";
-
-        //--- Footer include
-        include"includes/layouts/footer.php" ?>
+    //--- Footer include
+    include"includes/layouts/footer.php"
+  ?>
