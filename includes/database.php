4e1f3fe544111f2111ea4cd5e43b88c996a40b48
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-15 12:16:53 +0100

    add link database

--- includes/database.php
+++ includes/database.php
@@ -4,7 +4,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "youcode_tableauscrumbackend";
+    $dbname = "youcode_gestiongamingproduit";
 
     // Create connection
     $conn = new mysqli($servername, $username, $password , $dbname);
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php 
+    include('includes/scripts.php');
+?>
 <!doctype html>
 <html lang="en">
   <head>
