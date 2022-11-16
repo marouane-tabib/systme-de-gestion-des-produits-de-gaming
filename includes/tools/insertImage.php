1e35eb504def8ccb2375fc8d92f8415cfee0376e
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-16 12:02:09 +0100

    add insert image function - work out

---
+++ includes/tools/insertImage.php
@@ -0,0 +1,22 @@
+<?php 
+ function inserImage($file){
+    if(!empty($file["image"]["name"])) { 
+        // Get file info 
+        $fileName = basename($file["image"]["name"]); 
+        $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
+         
+        // Allow certain file formats 
+        $allowTypes = array('jpg','png','jpeg','gif'); 
+        if(in_array($fileType, $allowTypes)){ 
+            $image = $file['image']['tmp_name']; 
+            $imgContent = addslashes(file_get_contents($image)); 
+        }else{ 
+            $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.';
+        } 
+    }else{ 
+        $statusMsg = 'Please select an image file to upload.'; 
+    }
+    $result = $imgContent;
+            return $result;
+}
+?>
