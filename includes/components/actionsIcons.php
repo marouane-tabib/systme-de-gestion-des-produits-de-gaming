b016cf060f3511b22cf4f0bd7d3b98f8e917b543
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-17 13:20:04 +0100

    add action icons , delete and update - completed

--- includes/components/actionsIcons.php
+++ includes/components/actionsIcons.php
@@ -1,11 +1,13 @@
+    <div class="d-flex justify-content-around">
         <a href="javascript:void(0)"
             onclick="if(confirm('Are You sure to delete this record?')){document.querySelector('#delete-task-<?php echo $row['id'] ?>').submit();} else {return false}"
-            class="mx-2 text-danger" id="delete-btn">
+            class="text-danger" id="delete-btn">
                 <i class="fa-solid fa-trash"></i>
         </a>
-        <a href="update.php?id=<?php echo $row["id"]?>" class="mx-2 text-success">
+        <a href="update.php?id=<?php echo $row["id"]?>" class="text-success">
             <i class="fa-solid fa-pen-to-square"></i>
         </a>
+    </div>
         
     <form action="includes/scripts.php" method="post" class="d-none" id="delete-task-<?php echo $row['id'] ?>" >
         <input type="hidden" name="delete" value="<?php echo $row['id'] ?>" >
