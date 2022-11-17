b1ae0d991ddce716b0980d409a50757728c6145b
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-17 11:38:27 +0100

    add messages files - work out

---
+++ includes/tools/messages.php
@@ -0,0 +1,10 @@
+<?php 
+$messages = [
+    'succ'  => $_SESSION['action'] = [
+                    'status' => "Problem !",
+                    'message' => "Please Chek Your products details ... ",
+                    'class' => "alert alert-danger alert-dismissible fade show",
+                    'btnFade' => 1,
+                ] ,
+    'error' => '',];
+?>
