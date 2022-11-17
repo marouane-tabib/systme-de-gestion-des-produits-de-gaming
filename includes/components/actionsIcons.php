e5001d0100059713f25704a34037b864feb76607
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-17 13:22:29 +0100

    action icon - refactoring

--- includes/components/actionsIcons.php
+++ includes/components/actionsIcons.php
@@ -1,6 +1,6 @@
     <div class="d-flex justify-content-around">
         <a href="javascript:void(0)"
-            onclick="if(confirm('Are You sure to delete this record?')){document.querySelector('#delete-task-<?php echo $row['id'] ?>').submit();} else {return false}"
+            onclick="if(confirm('Are You sure to delete this record?')){document.querySelector('#delete-product-<?php echo $row['id'] ?>').submit();} else {return false}"
             class="text-danger" id="delete-btn">
                 <i class="fa-solid fa-trash"></i>
         </a>
@@ -9,6 +9,6 @@
         </a>
     </div>
         
-    <form action="includes/scripts.php" method="post" class="d-none" id="delete-task-<?php echo $row['id'] ?>" >
+    <form action="includes/scripts.php" method="post" class="d-none" id="delete-product-<?php echo $row['id'] ?>" >
         <input type="hidden" name="delete" value="<?php echo $row['id'] ?>" >
     </form>
