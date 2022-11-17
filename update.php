351293a76828197eaf15a503b1d61bcb691bc142
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-17 15:45:07 +0100

    add input value to updated - work out

--- update.php
+++ update.php
@@ -1,5 +1,6 @@
 <?php
     $title ="Update"; include"includes/components/header.php" ;
+    $item = getProduct($conn);
     if(!isset($_SESSION['user'])){ header('location:login.php'); die(); }
 ?>
         <div class="card-box container my-2 mx-auto p-3 shadow shadow-md align-item-center bg-white">
@@ -7,15 +8,15 @@
         <div class="modal-body">
             <div class="mb-3">
               <label for="product-image" class="form-label">Select Image File</label>
-              <input type="file" class="form-control" id="product-image" name="image" required>
+              <input type="file" value="assets/images/products/<?php echo $item['image'] ?>" class="form-control" id="product-image" name="image" required>
             </div>
             <div class="mb-3">
               <label for="name" class="form-label">Product Name</label>
-              <input type="text" class="form-control" id="name" name="name" placeholder="Add product name..." required>
+              <input type="text" value="<?php echo $item['name'] ?>" class="form-control" id="name" name="name" placeholder="Add product name..." required>
             </div>
             <div class="mb-3">
               <label for="quantity" class="form-label">Quantity</label>
-              <input type="text" class="form-control" id="quantity" name="quantity" placeholder="Add product quantity" required>
+              <input type="text" value="<?php echo $item['quantity'] ?>" class="form-control" id="quantity" name="quantity" placeholder="Add product quantity" required>
             </div>
             <div class="mb-3">
               <label for="platform" class="form-label">Platform</label>
@@ -27,11 +28,11 @@
             </div>
             <div class="mb-3">
               <label for="price" class="form-label">Price</label>
-              <input type="text" name="price" id="price" class="form-control" placeholder="Add product price" required>
+              <input type="text" value="<?php echo $item['price'] ?>" name="price" id="price" class="form-control" placeholder="Add product price" required>
             </div>
             <div class="mb-3">
               <label for="description" class="form-label">Description</label>
-              <textarea name="description" id="description" class="form-control" rows="3" required></textarea>
+              <textarea name="description" id="description" class="form-control" rows="3" required><?php echo $item['description'] ?></textarea>
             </div>
         </div>
         <div class="modal-footer">
