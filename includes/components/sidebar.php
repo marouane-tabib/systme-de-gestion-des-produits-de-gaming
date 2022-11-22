b7bf1c62f21717099afdace0e831ceebc332fa5c
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-22 17:21:05 +0100

    add condestion , if user not login , sidebar hidden

--- includes/components/sidebar.php
+++ includes/components/sidebar.php
@@ -1,4 +1,4 @@
-<div class="side-bar-box col-md-2 col-2 bg-light shadow shadow-sm">
+<div class="side-bar-box col-md-2 col-2 bg-light shadow shadow-sm <?php if (!isset($_SESSION['user'])) { echo 'd-none';} ?>">
   <div class="row bg-white py-3 px-2 my-2 shadow shadow-sm">
     <a href="index.php">
       <span class="icon col-md-3 col-12"><i class="fa fa-lg fa-home"></i></span>
