eab6601e1d744bdfdda01854bdc51ddc152559c4
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-22 10:52:06 +0100

    add shadow to side bar

--- includes/components/sidebar.php
+++ includes/components/sidebar.php
@@ -1,4 +1,4 @@
-<div class="side-bar-box col-md-2 col-2 bg-white">
+<div class="side-bar-box col-md-2 col-2 bg-white shadow shadow-md">
   <div class="row bg-light py-3 px-2 my-2 shadow shadow-sm">
     <a href="index.php">
       <span class="icon col-md-3 col-12"><i class="fa fa-lg fa-home"></i></span>
