c4d9d7839a279a4a0b7afed00fdaa3686af44685
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-22 10:47:43 +0100

    add side bar style - refactoring

--- includes/components/header.php
+++ includes/components/header.php
@@ -8,53 +8,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/main.css">
     <title><?php echo $title ?></title>
   </head>
   <body>
     <style>
-      .container-box{
-        display: flex;
-        flex-direction: column;
-        height: 100vh;
-        background-color: green; 
-      }
-      .container-box .content-container{
-        background-color: goldenrod;
-        height: 100%;
-      }
-      .container-box .nav-bar-box{
-        background-color: greenyellow;
-        /* height: 8vh; */
-      }
-      .container-box .side-bar-box{
-        background-color: blue;
-        overflow: hidden;
-        white-space: nowrap;
-        transition:.3s;
-      }
-      .container-box .side-bar-box .row:hover{
-        filter:invert(1);
-        cursor: pointer;
-        transition:.5s;
-      }
-      .container-box .side-bar-box .row{
-        flex-wrap: nowrap;
-      }
-      .container-box .side-bar-box .row a{
-        color: black;
-        text-decoration: none;
-        display: contents;
-      }
-      .container-box .side-bar-box .row .icon{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      .container-box .content-box{
-        overflow: auto;
-        background-color: red;
-      }
     </style>
     <div class="container-box">
       <!-- Nav Bar - Component -->
