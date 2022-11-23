561e5f80760c1851df9adde47c2248dedb247d31
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-23 13:31:43 +0100

    add statistique style card

--- index.php
+++ index.php
@@ -9,35 +9,6 @@
     $products = getproducts($conn);
     $platforms = getPlatforms($conn); ?> 
     
-    <style>
-      .calc-card{
-        padding: 15px 15px;
-        margin: 20px 5px;
-        border-radius: 7px;
-        position: relative;
-        background: #333;
-        color: #F8F8F8;
-      }
-      .calc-icon{
-    position: absolute;
-    top: -15px;
-    left: 0;
-    border-radius: 50%;
-    border: 0.6px solid #CCC;
-    clip-path: circle;
-    padding: 5px 9px;
-    background: #f8f8f8;
-    color: #333333;
-    box-shadow: 2px 1px 10px #0005 inset !important;
-}
-      .calc-sum{
-        text-align: center;
-    display: flex;
-    justify-content: center;
-    font-weight: bold;
-    font-size: 1.2rem;
-      }
-    </style>
     <div class="container row my-1 mx-auto">
       <div class="col-12 my-3 col-md shadow shadow-md calc-card">
         <span class="calc-icon">
