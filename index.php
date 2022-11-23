e7c4020ae7e79858cefd3999045258b6af613bc1
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-23 14:20:40 +0100

    add statistique component page - work out

--- index.php
+++ index.php
@@ -7,45 +7,9 @@
     // Redirect
     if(!isset($_SESSION['user'])){ header('location:login.php'); die(); }
     $products = getproducts($conn);
-    $platforms = getPlatforms($conn); ?> 
-    
-    <div class="container row my-1 mx-auto">
-      <div class="col-12 my-3 col-md shadow shadow-md calc-card">
-        <span class="calc-icon">
-          <i class="fa-solid fa-wallet"></i>
-        </span>
-        <span class="calc-sum">
-          344$
-        </span>
-      </div>
-      <div class="col-12 my-3 col-md shadow shadow-md calc-card">
-        <span class="calc-icon">
-          <i class="fa-solid fa-gamepad"></i>
-        </span>
-        <span class="calc-sum">
-          344$
-        </span>
-      </div>
-      <div class="col-12 my-3 col-md shadow shadow-md calc-card">
-        <span class="calc-icon">
-        <i class="fa-solid fa-box-open"></i>
-        </span>
-        <span class="calc-sum">
-          344$
-        </span>
-      </div>
-      <div class="col-12 my-3 col-md shadow shadow-md calc-card">
-        <span class="calc-icon">
-          <i class="fa-brands fa-lg fa-dribbble"></i>
-        </span>
-        <span class="calc-sum">
-          344$
-        </span>
-      </div>
-    </div>
-    
-    <?php
+    $platforms = getPlatforms($conn);
 
+    include'includes/components/statistique.php';
     //--- Body includes
       include"includes/components/model.php"; 
       include"includes/components/products_table.php";
