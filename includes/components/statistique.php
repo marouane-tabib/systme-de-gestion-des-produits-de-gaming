60b082e44760f975f5eddc5c405553fcd2f8b561
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-23 15:53:08 +0100

    add deynamic statistique card - refactoring

--- includes/components/statistique.php
+++ includes/components/statistique.php
@@ -38,36 +38,4 @@
                 </span>
             </div>
         <?php } ?>
-      <!-- <div class="col-12 my-3 col-md shadow shadow-md calc-card">
-        <span class="calc-icon">
-          <i class="fa-solid fa-wallet"></i>
-        </span>
-        <span class="calc-sum">
-          <?php $result = calc($conn , "SUM" , "price"); echo $result['calc'] ?> $
-        </span>
-      </div>
-      <div class="col-12 my-3 col-md shadow shadow-md calc-card">
-        <span class="calc-icon">
-          <i class="fa-solid fa-gamepad"></i>
-        </span>
-        <span class="calc-sum">
-          <?php $result = calc($conn , "SUM" , "quantity"); echo $result['calc'] ?>
-        </span>
-      </div>
-      <div class="col-12 my-3 col-md shadow shadow-md calc-card">
-        <span class="calc-icon">
-        <i class="fa-solid fa-box-open"></i>
-        </span>
-        <span class="calc-sum">
-          <?php $result = calc($conn , "COUNT" , "*"); echo $result['calc'] ?>
-        </span>
-      </div>
-      <div class="col-12 my-3 col-md shadow shadow-md calc-card">
-        <span class="calc-icon">
-          <i class="fa-brands fa-lg fa-dribbble"></i>
-        </span>
-        <span class="calc-sum">
-          <?php $result = calc($conn , "COUNT" , "platform_id"); echo $result['calc'] ?>
-        </span>
-      </div> -->
     </div>
