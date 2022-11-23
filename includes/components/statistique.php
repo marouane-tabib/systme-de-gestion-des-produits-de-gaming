74b62b2120b32263e914a87fbf9c159d6615334e
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-23 15:38:14 +0100

    add statistique component page - refactoring

--- includes/components/statistique.php
+++ includes/components/statistique.php
@@ -1,11 +1,13 @@
-
+<?php 
+    include('includes/tools/calc.php');
+?>
     <div class="container row my-1 mx-auto">
       <div class="col-12 my-3 col-md shadow shadow-md calc-card">
         <span class="calc-icon">
           <i class="fa-solid fa-wallet"></i>
         </span>
         <span class="calc-sum">
-          344$
+          <?php $result = calc($conn , "SUM" , "price"); echo $result['calc'] ?> $
         </span>
       </div>
       <div class="col-12 my-3 col-md shadow shadow-md calc-card">
@@ -13,7 +15,7 @@
           <i class="fa-solid fa-gamepad"></i>
         </span>
         <span class="calc-sum">
-          344$
+          <?php $result = calc($conn , "SUM" , "quantity"); echo $result['calc'] ?>
         </span>
       </div>
       <div class="col-12 my-3 col-md shadow shadow-md calc-card">
@@ -21,7 +23,7 @@
         <i class="fa-solid fa-box-open"></i>
         </span>
         <span class="calc-sum">
-          344$
+          <?php $result = calc($conn , "COUNT" , "*"); echo $result['calc'] ?>
         </span>
       </div>
       <div class="col-12 my-3 col-md shadow shadow-md calc-card">
@@ -29,7 +31,7 @@
           <i class="fa-brands fa-lg fa-dribbble"></i>
         </span>
         <span class="calc-sum">
-          344$
+          <?php $result = calc($conn , "COUNT" , "platform_id"); echo $result['calc'] ?>
         </span>
       </div>
     </div>
--- includes/tools/calc.php
+++ includes/tools/calc.php
@@ -1,6 +1,8 @@
 <?php 
-    function calc($conn , $action = "SUM" , $target = "price"){
-        $sql = "SELECT $action($target) as calc FROM products";
+    function calc($conn , $action , $target){
+        $target == "platform_id" ? $target = "DISTINCT ".$target : $target;
+        $sql = "SELECT $action($target) as calc FROM products 
+        INNER JOIN platforms ON products.platform_id = platforms.id";
 
         $result = $conn->query($sql);
         $result = $result->fetch_assoc();
