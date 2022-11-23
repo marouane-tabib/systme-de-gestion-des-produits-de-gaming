689e5e96dd37708c564890cc3d9302b411e0e01b
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-23 14:19:57 +0100

    add statistique component page - work out

---
+++ includes/components/statistique.php
@@ -0,0 +1,35 @@
+
+    <div class="container row my-1 mx-auto">
+      <div class="col-12 my-3 col-md shadow shadow-md calc-card">
+        <span class="calc-icon">
+          <i class="fa-solid fa-wallet"></i>
+        </span>
+        <span class="calc-sum">
+          344$
+        </span>
+      </div>
+      <div class="col-12 my-3 col-md shadow shadow-md calc-card">
+        <span class="calc-icon">
+          <i class="fa-solid fa-gamepad"></i>
+        </span>
+        <span class="calc-sum">
+          344$
+        </span>
+      </div>
+      <div class="col-12 my-3 col-md shadow shadow-md calc-card">
+        <span class="calc-icon">
+        <i class="fa-solid fa-box-open"></i>
+        </span>
+        <span class="calc-sum">
+          344$
+        </span>
+      </div>
+      <div class="col-12 my-3 col-md shadow shadow-md calc-card">
+        <span class="calc-icon">
+          <i class="fa-brands fa-lg fa-dribbble"></i>
+        </span>
+        <span class="calc-sum">
+          344$
+        </span>
+      </div>
+    </div>
