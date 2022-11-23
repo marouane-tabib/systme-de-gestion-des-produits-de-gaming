6fc0dea496bcf49855a53e173c56af675275a67d
marwan-tabib <marwanmaroc297@gmail.com>
2022-11-23 15:52:15 +0100

    add deynamic statistique card - refactoring

--- includes/components/statistique.php
+++ includes/components/statistique.php
@@ -1,8 +1,44 @@
 <?php 
     include('includes/tools/calc.php');
+    $statistique = [
+        [
+            "title" => "Sum Price",
+            "class_icon" => "fa-solid fa-wallet",
+            "action" => "SUM" ,
+            "target" => "price"
+        ],
+        [
+            "title" => "Sum Quantity",
+            "class_icon" => "fa-solid fa-gamepad",
+            "action" => "SUM" ,
+            "target" => "quantity"
+        ],
+        [
+            "title" => "Total",
+            "class_icon" => "fa-solid fa-box-open",
+            "action" => "COUNT" ,
+            "target" => "*"
+        ],
+        [
+            "title" => "Total",
+            "class_icon" => "fa-brands fa-lg fa-dribbble",
+            "action" => "COUNT" ,
+            "target" => "platform_id"
+        ],
+    ];
 ?>
     <div class="container row my-1 mx-auto">
-      <div class="col-12 my-3 col-md shadow shadow-md calc-card">
+        <?php foreach ($statistique as $card) { ?>
+            <div class="col-12 my-3 col-md shadow shadow-md calc-card">
+                <span class="calc-icon">
+                <i class="<?= $card['class_icon'] ?>"></i>
+                </span>
+                <span class="calc-sum">
+                <?php $result = calc($conn , $card['action'] , $card['target']); echo $result['calc'] ?>
+                </span>
+            </div>
+        <?php } ?>
+      <!-- <div class="col-12 my-3 col-md shadow shadow-md calc-card">
         <span class="calc-icon">
           <i class="fa-solid fa-wallet"></i>
         </span>
@@ -33,5 +69,5 @@
         <span class="calc-sum">
           <?php $result = calc($conn , "COUNT" , "platform_id"); echo $result['calc'] ?>
         </span>
-      </div>
+      </div> -->
     </div>
